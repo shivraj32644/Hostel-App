b9d3dfffa67abda1170777ca050e8da684b0fa5c
shivraj32644 <shivraj32644@gmail.com>
2022-12-04 17:14:30 +0530

    basic login page

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,20 +1,40 @@
-import { Button, Input, Box } from "@chakra-ui/react";
+import { Button, Input, Box, Heading } from "@chakra-ui/react";
 import React from "react";
 import { Navbar } from "../Components/Navbar";
-
+import { Highlight } from "@chakra-ui/react";
+import logo from '../Assets/hostel.png'
 export const Login = () => {
   return (
     <>
       <Navbar></Navbar>
-      <Box display="flex"
-      alignItems={'center'}
-        justifyContent={'center'}
-        height='90vh'
-        border={'1px solid black'}
+      <Box
+        display="flex"
+        alignItems={"center"}
+        justifyContent={"center"}
+        height="90vh"
+        border={"5px solid black"}
       >
-        <Input placeholder="Enter Email" />
-        <Input placeholder="Enter Password" />
-        <Button>Login</Button>
+        <Box>
+          <Box
+          
+            display={'flex'}
+            alignItems='center'
+            justifyContent={'center'}
+          >
+              <img src={logo} alt="logo" />
+            <Heading lineHeight="tall">
+              <Highlight
+                query="Hostel Buddy"
+                styles={{ px: "2", py: "1", rounded: "full", color: " #25acca " }}
+              >
+                Hostel Buddy
+              </Highlight>
+            </Heading>
+          </Box>
+          <Input placeholder="Enter Email" />
+          <Input placeholder="Enter Password" />
+          <Button>Login</Button>
+        </Box>
       </Box>
     </>
   );
