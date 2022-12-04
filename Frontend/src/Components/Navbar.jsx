afcd7fef38363a1d440a1784263fd2f44312647c
shivraj32644 <shivraj32644@gmail.com>
2022-12-04 16:41:20 +0530

    working on navbar

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,44 +1,37 @@
-import React from 'react'
-import { Box } from '@chakra-ui/react'
+import React from "react";
+import { Box } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
+import styles from '../styles/navbar.module.css'
 export const Navbar = () => {
   const data = [
     {
-    to: '/',
-    title:"Home"
-  },
+      to: "/",
+      title: "Home",
+    },
     {
-    to: '/register',
-    title:"Signup"
-  }
+      to: "/register",
+      title: "Signup",
+    },
     {
-    to: '/about us',
-    title:"About Us"
-  },
+      to: "/about us",
+      title: "About Us",
+    },
     {
-    to: '/contact',
-    title:"Contact"
-  }
-  
-  ]
+      to: "/contact",
+      title: "Contact",
+    },
+  ];
   return (
     <Box
-      display={'flex'}
-      alignItems='center'
-      height={'60px'}
-      boxShadow='outline'
-      justifyContent={'space-evenly'}
-    
+      display={"flex"}
+      alignItems="center"
+      height={"60px"}
+      boxShadow="outline"
+      justifyContent={"space-evenly"}
     >
-      {
-        data?.map((e) => (
-          <NavLink to={e.to} >
-            {e.title}
-          </NavLink>
-        ))
-      }
-      
-      
-      </Box>
-  )
-}
+      {data?.map((e) => (
+        <NavLink to={e.to} className={({isActive})=>isActive? styles.active :styles.default} >{e.title}</NavLink>
+      ))}
+    </Box>
+  );
+};
