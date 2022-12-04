dae402a7d8a0abdc6eb42d1de006e0bf4d23e201
shivraj32644 <shivraj32644@gmail.com>
2022-12-04 17:21:07 +0530

    basic login page

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Navbar } from "../Components/Navbar";
 import { Highlight } from "@chakra-ui/react";
 import logo from '../Assets/hostel.png'
+import { Divider } from '@chakra-ui/react'
 export const Login = () => {
   return (
     <>
@@ -31,9 +32,33 @@ export const Login = () => {
               </Highlight>
             </Heading>
           </Box>
-          <Input placeholder="Enter Email" />
-          <Input placeholder="Enter Password" />
-          <Button>Login</Button>
+          <Box>
+            <h1>Login</h1>
+            <Link to='/register'>No Account Yet ?</Link>
+          </Box>
+          <Box>
+            <Input placeholder="Enter Email" />
+            <Input placeholder="Enter Password" />
+            <Box>
+              <p>Forgot Password</p>
+            </Box>
+            <Button>Login</Button>
+
+
+          </Box>
+          <Box>
+            <Box>
+              <Divider />
+              <p>Login with</p>
+              <Divider />
+            </Box>
+
+          </Box>
+          <Box>
+            <Button>Github</Button>
+            <Button>Google</Button>
+            <Button>Facebook</Button>
+          </Box>
         </Box>
       </Box>
     </>
